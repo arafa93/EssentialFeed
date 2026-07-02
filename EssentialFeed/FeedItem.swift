a77fb6d404e7ea57a73d65297e5934ac9edb7fe7
mohamed.arafa <mohamed.arafa@1pass.app>
2026-07-02 17:02:34 +0300

    Create FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mohamed Arafa on 02/07/2026.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

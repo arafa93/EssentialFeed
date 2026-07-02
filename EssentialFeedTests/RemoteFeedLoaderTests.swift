93bafc207a5781a2171c7d11618a073aecd3ec9f
mohamed.arafa <mohamed.arafa@1pass.app>
2026-07-02 19:48:53 +0300

    Factory test to remove dublications

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,6 +7,10 @@
 
 import XCTest
 
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
 class RemoteFeedLoader {
     
     let client: HTTPClient
@@ -21,25 +25,11 @@ class RemoteFeedLoader {
     }
 }
 
-protocol HTTPClient {
-    func get(from url: URL)
-}
-
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        
-        let url = URL(string: "https://dummy.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
@@ -47,12 +37,28 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_requestsDataFromURL() {
         
         let url = URL(string: "https://a-dummy.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
     }
-
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://dummy.com")!) -> (sut: RemoteFeedLoader,
+                                                                           client: HTTPClientSpy) {
+        
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut: sut, client: client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
 }

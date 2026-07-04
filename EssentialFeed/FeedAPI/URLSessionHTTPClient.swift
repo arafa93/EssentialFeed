547e117f4141f6e58cbd1ffa2b20e38a107ce889
mohamed.arafa <mohamed.arafa@1pass.app>
2026-07-04 21:21:59 +0300

    Move  it's own file in production

---
+++ EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -0,0 +1,30 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Mohamed Arafa on 04/07/2026.
+//
+
+import Foundation
+
+class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    struct UnexpectedValuesRepresentation: Error {}
+    
+    func get(from url: URL, completion: @escaping HTTPClientResultCompletion) {
+        session.dataTask(with: url) { data, response, error in
+            if let error {
+                completion(.failure(error))
+            } else if let data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
